81936212c4786ecb8666d4b5556b13645d599a8b
Oubayda Khayata <oubaydakhayata@gmail.com>
2020-07-31 21:28:29 +0300

    Add CreateAppointmentRequest custom request class

---
+++ app/Http/Requests/CreateAppointmentRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class CreateAppointmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|string',
+            'expert_id' => 'required|numeric',
+            'appointment_duration_id' => 'required|numeric',
+            'datetime' => 'required|numeric'
+        ];
+    }
+
+    /**
+    * Handle a failed validation attempt.
+    *
+    * @param  \Illuminate\Contracts\Validation\Validator  $validator
+    * @return void
+    *
+    * @throws \Illuminate\Validation\ValidationException
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(json('error', $validator->errors()->all(), '', 400));
+    }
+}
